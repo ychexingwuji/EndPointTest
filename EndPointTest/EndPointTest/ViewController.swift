410b850f64709e8fcd2c2e92bb17d68b92e3abcf
yangwei <ychexingwuji@163.com>
2021-03-02 22:56:43 +0800

    引入 Alamofire 并使用 textView展示接口数据

--- EndPointTest/EndPointTest/ViewController.swift
+++ EndPointTest/EndPointTest/ViewController.swift
@@ -7,14 +7,48 @@
 //
 
 import UIKit
+import Alamofire
 
 class ViewController: UIViewController {
-
+    
+    var textView: UITextView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        initUI()
+        
+        reqData()
     }
-
-
+    
+    func initUI() {
+        // textView
+        let sideSpace = CGFloat(20.0)
+        let contentWidth = UIScreen.main.bounds.size.width - 2 * sideSpace
+        let contentHeight = UIScreen.main.bounds.size.height - 2 * sideSpace
+        textView = UITextView(frame: CGRect(x: sideSpace, y: sideSpace, width: contentWidth , height: contentHeight))
+        
+        self.view.addSubview(textView)
+    }
+    
+    func reqData() {
+        Alamofire.AF.request("https://api.github.com/").responseJSON { (response) in
+            switch response.result {
+            case .success(let json):
+                
+                if JSONSerialization.isValidJSONObject(json) {
+                    let data = try!JSONSerialization.data(withJSONObject: json, options: .prettyPrinted)
+                    let jsonStr = String(data: data, encoding: .utf8)! as String
+                    self.textView.text = jsonStr
+                }
+                
+                print(json)
+                break
+            case .failure(let error):
+                print("error:\(error)")
+                break
+            }
+        }
+    }
 }
 
